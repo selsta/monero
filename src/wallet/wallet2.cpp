955d991c8828cc149bb716b41d2ded341ec4d3eb
selsta <selsta@sent.at>
2026-09-13 20:12:04 +0200

    wallet2: validate public node addresses before display

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -16219,6 +16219,15 @@ std::vector<cryptonote::public_node> wallet2::get_public_nodes(bool white_only)
   nodes = res.white;
   nodes.reserve(nodes.size() + res.gray.size());
   std::copy(res.gray.begin(), res.gray.end(), std::back_inserter(nodes));
+  for (auto &node: nodes)
+  {
+    THROW_WALLET_EXCEPTION_IF(node.rpc_port == 0,
+        error::wallet_internal_error, "Invalid public node address from daemon");
+    const auto address = net::get_network_address(node.host, node.rpc_port);
+    THROW_WALLET_EXCEPTION_IF(!address || (node.host != address->host_str() && node.host != address->str()),
+        error::wallet_internal_error, "Invalid public node address from daemon");
+    node.host = address->host_str();
+  }
   return nodes;
 }
 //----------------------------------------------------------------------------------------------------
